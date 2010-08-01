d2a486ae2d2f6918bf28be560f24af866ba85011
Brad Jasper <bjasper@gmail.com>
2010-07-31 22:05:18 -0500

    updated redis list producer and consumer

--- redis_consumer.py
+++ redis_consumer.py
@@ -1,5 +1,6 @@
 import redis
 import json 
+import urllib
 
 db = redis.Redis()
 
@@ -7,3 +8,8 @@ while True:
     key, msg = db.blpop("redis_queue")
     data = json.loads(msg)
     db.incr(data["test_name"])
+
+    print data["download_url"]
+    url = urllib.urlopen(data["download_url"])
+    print url.read()
+    db.set("download_url", data["download_url"])
--- redis_producer.py
+++ redis_producer.py
@@ -1,12 +1,23 @@
 import time
+import random
 import json
 
 import redis
 
 db = redis.Redis()
-num = 100000
+num = 500
 
 test_name = "concurrency_test_%d" % db.incr("concurrency_num_tests")
+urls = [
+    "http://www.rentacarnow.com/",
+    "http://www.google.com",
+    "http://www.yahoo.com",
+    "http://www.msn.com",
+    "http://www.nytimes.com",
+    "http://news.google.com",
+    "http://microsoft.com",
+    "http://amazon.com"
+]
 
 print "Running test %s at %d messages" % (test_name, num)
 
@@ -14,7 +25,7 @@ start_time = time.time()
 
 for i in xrange(num):
     db.lpush("redis_queue", json.dumps({
-        "download_url": "http://cnn.com/rss/edition.rss",
+        "download_url": random.choice(urls),
         "rate_id": "243234-234234234-234234234234-234234234234",
         "search_id": "234234234-234234234234-234234234-234234234",
         "test_name": test_name,
@@ -28,6 +39,3 @@ while amount != num:
     amount = int(db.get(test_name) or 0)
 
 print "Done in %f seconds" % (time.time() - start_time)
-
-db.del(test_name)
-db.del("redis_queue")
